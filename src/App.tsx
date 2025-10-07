98e79327f69637efeae3cb55a84d3f517eb39e03
manta33 <sksmstmdals3@gmail.com>
2025-10-08 00:32:29 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 declare module "styled-components" {
   export interface DefaultTheme {
@@ -9,10 +9,28 @@ declare module "styled-components" {
 }
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
