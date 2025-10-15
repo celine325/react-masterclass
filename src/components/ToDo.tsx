72cf48db648b33d5ee50422dfdd16dfa2ba2c2c3
manta33 <sksmstmdals3@gmail.com>
2025-10-16 00:52:31 +0900

    6.15 Immutability

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,22 @@
 import { useActionState } from "react";
 import { IToDo, toDoAtom } from "../atoms";
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 
 function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useAtom(toDoAtom);
+  const setToDos = useSetAtom(toDoAtom);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
